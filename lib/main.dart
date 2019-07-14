e881ca1ec5ea7e27bf9b58d2d818a00341649222
alexandreKnob <alexandre.knob@gmail.com>
2019-07-14 10:39:39 -0300

    vai

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cadastro de Produtos"),
+        title: Text("Teste Google FireStore "),
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
